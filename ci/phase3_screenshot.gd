79ae5f692acbf103b02ded9dc15014d29ab9777c
Fars12321 <156544769+Fars12321@users.noreply.github.com>
2026-09-09 22:42:24 +0300

    fix: make screenshot runner frame-driven

--- ci/phase3_screenshot.gd
+++ ci/phase3_screenshot.gd
@@ -1,14 +1,20 @@
 extends SceneTree
 
+var frames := 0
+
 func _init() -> void:
 	var main_scene: PackedScene = load("res://scenes/Main.tscn") as PackedScene
 	assert(main_scene != null, "Main.tscn could not be loaded")
 	var main: Node = main_scene.instantiate()
 	root.add_child(main)
-	await process_frame
-	await process_frame
-	await process_frame
+	process_frame.connect(_on_frame)
+
+func _on_frame() -> void:
+	frames += 1
+	if frames < 10:
+		return
 	var image: Image = root.get_viewport().get_texture().get_image()
-	image.save_png("artifacts/phase3.png")
+	var err := image.save_png("artifacts/phase3.png")
+	assert(err == OK, "Could not save screenshot")
 	print("PHASE3_SCREENSHOT_PASS")
 	quit(0)
